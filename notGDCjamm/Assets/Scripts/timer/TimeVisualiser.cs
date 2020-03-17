6b8810d3678150008902cd51a2bb37f4e6148eb0
nikinov <novellenicholas@gmail.com>
2020-03-18 00:10:50 +0100

    adding sorps stuff + textMeshPro

---
+++ notGDCjamm/Assets/Scripts/timer/TimeVisualiser.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class TimeVisualiser : MonoBehaviour {
+    
+    private TMP_Text timerUI;
+
+    private void Awake()
+    {
+        timerUI = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        timerUI.text = (int) GlobalTimer.timer.Countdown + ":" +(int) (100*(GlobalTimer.timer.Countdown - (int) GlobalTimer.timer.Countdown));
+    }
+
+}
