c8fe5d50df96359d349861ee2142350f2e9e09e8
nikinov <novellenicholas@gmail.com>
2020-03-19 01:35:57 +0100

    anime + br fix

---
+++ notGDCjamm/Assets/Scripts/anime/catAnime.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class catAnime : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            animator.SetInteger("Side", 1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            animator.SetInteger("Side", 2);
+        }
+        else
+        {
+            animator.SetInteger("Side", 0);
+        }
+    }
+}
