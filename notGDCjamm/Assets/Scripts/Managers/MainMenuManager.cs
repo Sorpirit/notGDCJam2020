eb6552fab3fe977dfc7289596ec916b17af5337d
nikinov <novellenicholas@gmail.com>
2020-03-19 18:28:18 +0100

    UI stuff and more

---
+++ notGDCjamm/Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Animator animator;
+
+    public void GoToPlay(int SceneInt)
+    {
+        StartCoroutine(wait(SceneInt));
+    }
+    IEnumerator wait(int i)
+    {
+        animator.SetTrigger("close");
+        yield return new WaitForSeconds(.30f);
+        SceneManager.LoadScene(sceneBuildIndex: i+1);
+    }
+}
