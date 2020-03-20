5673bb9f3402cebd0e15d8d9c592b186d5cbf5b6
nikinov <novellenicholas@gmail.com>
2020-03-20 20:18:51 +0100

    countDown

--- notGDCjamm/Assets/Scripts/Managers/GameManager.cs
+++ notGDCjamm/Assets/Scripts/Managers/GameManager.cs
@@ -6,11 +6,17 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public List<Sprite> iceImages;
+    public List<Sprite> countDown;
+    public Image Cimage;
     public Image image;
+    public GameObject Player;
+    public GameObject Cpanel;
+    public Animator animator;
 
     private void Start()
     {
-
+        Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        StartCoroutine(wait());
     }
     private void Update()
     {
@@ -21,6 +27,22 @@ public class GameManager : MonoBehaviour
         float b = (a / 100) * 8;
 
         image.sprite = iceImages[(int)b];
+    }
+    IEnumerator wait()
+    {
+        Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        yield return new WaitForSeconds(.30f);
+        Cimage.sprite = countDown[0];
+        yield return new WaitForSeconds(1);
+        Cimage.sprite = countDown[1];
+        yield return new WaitForSeconds(1);
+        Cimage.sprite = countDown[2];
+        yield return new WaitForSeconds(1);
+        Cimage.sprite = countDown[3];
+        Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        Player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        yield return new WaitForSeconds(1);
+        Cpanel.SetActive(false);
     }
 
 }
