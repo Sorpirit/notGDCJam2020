e25beba8538e9d3f44a5a70859b73f0971844a58
nikinov <novellenicholas@gmail.com>
2020-03-21 22:14:12 +0100

    importing script

---
+++ notGDCjamm/Assets/Scripts/audio/AudioManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private bool isInited;
+
+    private void Awake()
+    {
+        if (!isInited) Init();
+    }
+
+    public void Init(){
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+
+        isInited = true;
+    }
+
+    public void PlaySound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+        if(s == null)
+        {
+            Debug.Log("the sound" + name + "Has not been found");
+            return;
+        }
+    }
+    public void StopSound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Stop();
+        if (s == null)
+        {
+            Debug.Log("the sound" + name + "Has not been found");
+            return;
+        }
+    }
+    public void SetAllsound(bool a)
+    {
+        if (a)
+        {
+            foreach (Sound s in sounds)
+            {
+                if(s.source == null){
+                    Debug.Log("Null");
+                    continue;
+                }
+                    
+
+                s.source.pitch = 1;
+            }
+        }
+        else if(!a)
+        {
+            foreach (Sound s in sounds)
+            {
+                if(s.source == null){
+                    Debug.Log("Null");
+                    continue;
+                }
+
+                s.source.pitch = 0;
+            }
+        }
+    }
+}
